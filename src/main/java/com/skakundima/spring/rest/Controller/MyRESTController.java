4b0a861976a54485d33ac65ba604f737725cad2c
Dmitrii Skakun <112588946+dI98Sk@users.noreply.github.com>
2023-06-03 18:23:47 +0300

    first commit

---
+++ src/main/java/com/skakundima/spring/rest/Controller/MyRESTController.java
@@ -0,0 +1,68 @@
+package com.skakundima.spring.rest.Controller;
+
+import com.skakundima.spring.rest.entity.Employee;
+import com.skakundima.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.skakundima.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.skakundima.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}") //
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null){
+            throw new NoSuchEmployeeException("This is no employee with ID = " +
+                    id + " in Database");
+        }
+
+        return employee;
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+
+        return  new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+
+        return  new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/skakundima/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.skakundima.spring.rest.dao;
+
+import com.skakundima.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
